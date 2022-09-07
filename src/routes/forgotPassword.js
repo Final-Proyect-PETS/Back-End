dcffc853ab5df6fd65c7412710cf5f8ef97b51dd
ManuMuller <manumu99@hotmail.com>
2022-09-06 22:40:48 -0300

    arreglado todo creo

--- src/routes/forgotPassword.js
+++ src/routes/forgotPassword.js
@@ -19,31 +19,42 @@ router.post("/forgotpassword", async (req, res, next) => {
                 message: "No existe ese email"
             })
         }
-
         let id = user._id;
         const token = jwt.sign({ id: id }, process.env.SECRET_KEY, { expiresIn: "1h" });
         res.json({ error: null, data: { token }, id: { id } });
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            service: "gmail",
+            port: 587,
+            secure: false,
             auth: {
                 user: "happytailshp@gmail.com",
                 pass: `${process.env.NMAILER_PASSWORD2}`,
             },
-        })
-        const emailPort = 587
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+        let link = `https://happytails.vercel.app/587/resetpassword/${id}/${token}`
+        let contentHTML = `
+        
+                     <h2>Hola!, ${user.first_name} ${user.last_name}. </h2> <br>
+        Haga click en <a href=${link}>este link</a> para recuperar su contraseña <br>
+
+          <img src = "https://cdn-icons-png.flaticon.com/512/194/194279.png" style="width:100px;"/> <br>
+
+
+                                        Atentamente HT`;
         const mailOptions = {
             from: "'HappyTails'<happytailshp@gmail.com>",
-            to: `${user.email}`,
-            subject: "Recuperar contraseña en Happy Tails",
-            text: `https://happytails.vercel.app/${emailPort}/resetpassword/${id}/${token}`
+            to: user.email,
+            subject: "Recuperar contraseña",
+            html: contentHTML,
         }
         transporter.sendMail(mailOptions, (err, response) => {
             if (err) {
                 console.error("Ha ocurrido un error", err);
             } else {
-                console.error("Response", response);
-                res.status(200).json("El email para la reucperacion ha sido enviado")
+                console.log("Response", response);
             }
         })
     } catch (error) {
