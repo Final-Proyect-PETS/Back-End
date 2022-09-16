c024b97c96b7e6705e799f0a24e49440481bd36f
Tomas <91784094+Tomas000000@users.noreply.github.com>
2022-09-16 12:18:39 -0300

    email

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -109,10 +109,51 @@ router.get(
     });
 
     try {
+      
       const oneUser = await User.findOne({ _id: idBuyer });
       const product = await Product.findOne({ _id: productId });
       const image = product.image[0];
+      const {
+        money,
+        first_name,
+        second_name,
+        logUser,
+        thing,
+        logUserEmail,
+      } = req.body;
 
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "happytailshp@gmail.com",
+          pass: `${NMAILER_PASSWORD2}`,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
+
+      let contentHTML = `
+  <img src = "https://cdn-icons-png.flaticon.com/512/194/194279.png" style="width:100px;"/>
+      <h1>Hola ${logUser.uername}!</h1>
+  <h2>El usuario <a href="https://happytails.vercel.app/users/${logUser._id}">${first_name} ${second_name}</a> compro ${thing}.
+              <h1> Ganancia: ${money}</h1>
+                        <p>Te deseamos un buen dia!</p>
+                              Atentamente HT`;
+
+
+
+      let info = await transporter.sendMail({
+        from: "'HappyTails'<happytailshp@gmail.com>",
+        to: logUserEmail,
+        subject: "Notificacion de venta",
+        html: contentHTML,
+      });
+
+      console.log("message sent", info.messageId);
+      res.send("OK");
       let preference = {
         items: [
           {
@@ -152,7 +193,8 @@ router.get(
         })
         .catch(function (error) {
           next(error);
-        });
+        })
+        
     } catch (error) {
       next(error);
     }
